62f465ec2275f8aaa86359f68176e9cccd303471
Siarhei Shauchenka <siarhei_shauchenka@epam.com>
2022-05-11 12:28:02 +0200

    reverse fix

--- task02-slice.go
+++ task02-slice.go
@@ -1,7 +1,7 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	result = make([]int64, len(input))
+	result = make([]int64, 0)
 	for i := len(input) - 1; i >= 0; i-- {
 		result = append(result, input[i])
 	}
